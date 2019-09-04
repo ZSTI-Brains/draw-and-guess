fcb52df35cf8c95852a1f3c6490ab574443653e4
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-09-04 09:53:54 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <div id="drawing-container"></div>
+        <div id="social-container"></div>
+    </main>
+</body>
+</html>
