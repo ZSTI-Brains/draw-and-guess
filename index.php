1992165522094f89209ff3d9927c4b0b09f6fd86
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-09-05 20:04:00 +0200

    Changed the index.html

--- index.php
+++ index.php
@@ -1,32 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <main>
-        <div class="row">
-            <div>
-                <div id="scoreboard-container"></div>
-            </div>
-        </div>
-        <div class="row">
-            <div>
-                <div id="drawing-container"></div>
-            </div>
-            <div>
-                <div id="social-container">
-                    <div id="chat-container">
-                        <div></div>
-                        <div>
-                            <input type="text">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<main>
+
+		</main>
+	</body>
 </html>
