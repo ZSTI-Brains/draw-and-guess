a03352cb8d1d9ee46008fc6af441da3a98e22692
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-09-06 18:54:29 +0200

    Changed a PHP and CSS, added get-message.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title></title>
+		<title>Draw and guess</title>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap">
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
@@ -13,8 +14,13 @@
 			<div class="row main">
 				<div id="drawing-container"></div>
 				<div>
-					<div></div>
-					<div></div>
+					<div id="word-container"></div>
+					<div id="chat-container">
+						<div id="chat-messages-container"></div>
+						<div id="chat-input-container">
+							<input type="text" placeholder="Type your thought here...">
+						</div>
+					</div>
 				</div>
 			</div>
 		</main>
