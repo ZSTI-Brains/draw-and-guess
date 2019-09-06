57d689ac015ddb6f57001420efbe454d915df4aa
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-09-06 19:40:51 +0200

    Changed PHP files

--- get-message.php
+++ get-message.php
@@ -1,3 +1,21 @@
 <?php
+    $db_server = "127.0.0.1";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "draw_and_guess";
 
+    $mysqli = new mysqli($db_server, $db_user, $db_pass, $db_name);
+
+    $query = "SELECT * FROM chat_messages";
+
+    if ($result = $mysqli->query($query)) {
+        $msg = array();
+
+        while ($row = $result->fetch_assoc())
+            $msg[] = $row;
+
+        echo json_encode($msg);
+    }
+
+    $mysqli->close();
 ?>
