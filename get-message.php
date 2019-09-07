444a5f84ef7650de3716e63314b0696c778db602
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-09-07 18:07:58 +0200

    Changed get-message.php

--- get-message.php
+++ get-message.php
@@ -4,21 +4,23 @@
     $db_pass = "";
     $db_name = "draw_and_guess";
 
-    $mysqli = new mysqli($db_server, $db_user, $db_pass, $db_name);
+    if (isset($_POST["messageId"])) {
+        $id = $_POST["messageId"];
+        $mysqli = new mysqli($db_server, $db_user, $db_pass, $db_name);
 
-    $query = "SELECT chat_messages.message, users.nickname FROM chat_messages INNER JOIN users ON chat_messages.user_id = users.id";
+        $query = "SELECT chat_messages.id, chat_messages.message, users.nickname FROM chat_messages
+                  INNER JOIN users ON chat_messages.user_id = users.id WHERE chat_messages.id > $id
+                  ORDER BY chat_messages.id";
 
-    if ($result = $mysqli->query($query)) {
-        /*$msg = array();
+        if ($result = $mysqli->query($query)) {
+            $msg = array();
 
-        while ($row = $result->fetch_assoc())
-            $msg[] = $row;
+            while ($row = $result->fetch_assoc())
+                $msg[] = $row;
 
-        echo json_encode($msg);*/
-        while ($row = $result->fetch_assoc())
-            echo "<div class=\"chat-message\"><p class=\"message\">" . $row["message"] .
-                 "</p><p class=\"nickname\">" . $row["nickname"] . "</p></div>";
+            echo json_encode($msg);
+        }
+
+        $mysqli->close();
     }
-
-    $mysqli->close();
 ?>
