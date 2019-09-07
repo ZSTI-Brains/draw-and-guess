e44133cb041986ffd4e508fd132006f18bc6e05d
Marcin <wojtasmarcin.20@gmail.com>
2019-09-07 22:22:32 +0200

    For repair

--- chat.js
+++ chat.js
@@ -15,25 +15,24 @@ function checkMessage() {
     })
     .done(function(response) {
         messages = JSON.parse(response);
-    
-        //console.log(id);
-        for (i = id; i < messages.length; i++) {
-            var mess = document.createElement("div");
-            var p_message = document.createElement("p");
-            var p_nickname = document.createElement("p");
-
-            p_message.innerText = messages[i].message;
-            p_nickname.innerText = messages[i].nickname;
-
-            mess.classList.add("chat-message");
-            p_nickname.classList.add("nickname");
-
-            mess.append(p_message);
-            mess.append(p_nickname);
-
-            chatMessagesContainer.appendChild(mess); 
-        }   
-        console.log(messages.length);
+        
+//        for (i = id; i < messages.length; i++) {
+//            var mess = document.createElement("div");
+//            var p_message = document.createElement("p");
+//            var p_nickname = document.createElement("p");
+//
+//            p_message.innerText = messages[i].message;
+//            p_nickname.innerText = messages[i].nickname;
+//
+//            mess.classList.add("chat-message");
+//            p_nickname.classList.add("nickname");
+//
+//            mess.append(p_message);
+//            mess.append(p_nickname);
+//
+//            chatMessagesContainer.appendChild(mess); 
+//        } 
+        
         id = messages.length;  
     })
     .fail(function() {
