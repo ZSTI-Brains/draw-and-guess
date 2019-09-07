3acbaf4509612363339290624bbcfbe977398e73
Marcin <wojtasmarcin.20@gmail.com>
2019-09-07 21:34:49 +0200

    Fuck

--- chat.js
+++ chat.js
@@ -1,10 +1,13 @@
 var chatMessagesContainer = document.querySelector("#chat-messages-container");
 var messages = [];
-var id = 6; //fixed
+var id;
 
 window.setInterval(checkMessage, 1000);
 
 function checkMessage() {
+    if(id == undefined)
+        id = 0;
+          
     $.ajax({
         type : "post",
         data : {messageId: id},
@@ -12,25 +15,26 @@ function checkMessage() {
     })
     .done(function(response) {
         messages = JSON.parse(response);
+    
+        //console.log(id);
         for (i = id; i < messages.length; i++) {
-            var message = document.createElement("div");
+            var mess = document.createElement("div");
             var p_message = document.createElement("p");
             var p_nickname = document.createElement("p");
-            
+
             p_message.innerText = messages[i].message;
             p_nickname.innerText = messages[i].nickname;
-            
-            message.classList.add("chat-message");
+
+            mess.classList.add("chat-message");
             p_nickname.classList.add("nickname");
-            
-            message.append(p_message);
-            message.append(p_nickname);
-            
-            chatMessagesContainer.appendChild(message); 
-        }
-        id = messages.length;
-        console.log(messages.length)
-        //console.log(id);
+
+            mess.append(p_message);
+            mess.append(p_nickname);
+
+            chatMessagesContainer.appendChild(mess); 
+        }   
+        console.log(messages.length);
+        id = messages.length;  
     })
     .fail(function() {
         alert("Error!");
