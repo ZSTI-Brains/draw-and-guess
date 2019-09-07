8f761846c6afc28d26621c7c7341e810c055d14e
Marcin <wojtasmarcin.20@gmail.com>
2019-09-07 19:30:12 +0200

    I tried to do something.

--- chat.js
+++ chat.js
@@ -1,22 +1,37 @@
 var chatMessagesContainer = document.querySelector("#chat-messages-container");
 var allMessages = document.querySelectorAll(".chat-message");
-var lastMessage;
-var howManyMessages;
+var messages = [];
+var m;
+var result = [];
+var j = 0;
 
 window.setInterval(checkMessage, 1000);
 
 function checkMessage() {
-    howManyMessages = allMessages.length;
-    theLast = howManyMessages - 1;
-    lastMessage = allMessages[theLast];
+    m = allMessages.length;
     
     $.ajax({
         type : "post",
-        //data        : {id: howManyMessages, lastMessage: lastMessage,},
+        data        : {messageId: m,},
         url : "get-message.php",
     })
     .done(function(response) {
-        chatMessagesContainer.innerHTML = response;
+//        console.log(response);
+//        console.log(response.length);
+        for(i = 0; i < response.length; i++){
+            if(response[i] != "\"" && response[i] != "[" && response[i] != "]" && response[i] != "{") {
+                result[j] += response[i];
+            }
+            if(response[i] == "}")
+                j++;
+        }
+        console.log(result);
+        
+        // message [id][message][nickname]
+        chatMessagesContainer.innerHTML = "<div class=\"chat-message\">" + result + "</div>";
+        //pushed = messages.push(response);
+        //chatMessagesContainer.innerHTML = messages;
+
     })
     .fail(function() {
         alert("Error!");
