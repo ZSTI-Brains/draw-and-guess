74f4340c5e04e23a4a86e9d1ca6c85afd4d0f223
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-09-09 20:38:34 +0200

    Updated a canvas drawing

--- js/draw.js
+++ js/draw.js
@@ -1,14 +1,19 @@
 var canvas = $("#drawing-canvas");
 canvas.mousemove((evt) => { draw(evt); });
 var ctx = document.getElementById("drawing-canvas").getContext("2d");
-
+var x, y, pX, pY;
+ctx.lineWidth = 4;
 function draw(evt) {
     let rect = evt.target.getBoundingClientRect();
+    pX = x;
+    pY = y;
     x = evt.clientX - rect.left;
     y = evt.clientY - rect.top;
 
     ctx.beginPath();
-    ctx.arc(x, y, 4, 0, Math.PI * 2, true);
-    ctx.fill();
+    ctx.moveTo(pX, pY);
+    ctx.lineTo(x, y);
+    ctx.closePath();
+    ctx.stroke();
 }
 //setInterval(draw, 10);
