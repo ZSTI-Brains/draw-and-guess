d9db2b0e75040dad308df43180dce6285d5fad56
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-09-10 20:46:49 +0200

    Small changes

--- js/draw.js
+++ js/draw.js
@@ -1,19 +1,38 @@
 var canvas = $("#drawing-canvas");
+canvas.mousedown((evt) => { startDraw(evt); });
 canvas.mousemove((evt) => { draw(evt); });
 var ctx = document.getElementById("drawing-canvas").getContext("2d");
 var x, y, pX, pY;
-ctx.lineWidth = 4;
-function draw(evt) {
-    let rect = evt.target.getBoundingClientRect();
-    pX = x;
-    pY = y;
-    x = evt.clientX - rect.left;
-    y = evt.clientY - rect.top;
+var pressed;
 
-    ctx.beginPath();
-    ctx.moveTo(pX, pY);
-    ctx.lineTo(x, y);
-    ctx.closePath();
-    ctx.stroke();
+function startDraw(evt) {
+    pressed = true;
+
+    let rect = evt.target.getBoundingClientRect();
+    x = pX = evt.clientX - rect.left;
+    y = pY = evt.clientY - rect.top;
+ctx.beginPath();
+    ctx.moveTo(x, y);
 }
-//setInterval(draw, 10);
+
+function draw(evt) {
+    if (pressed) {
+        let rect = evt.target.getBoundingClientRect();
+        x = evt.clientX - rect.left;
+        y = evt.clientY - rect.top;
+
+        if (x != pX || y != pY) {
+                    console.log(x);
+            ctx.lineWidth = 10;
+            //ctx.beginPath();
+            ctx.lineTo(x, y);
+            ctx.stroke();
+
+            pX = x;
+            pY = y;
+        }
+    }
+    //ctx.beginPath();
+    //ctx.arc(x, y, 10, 0, Math.PI * 2)
+    //ctx.fill();
+}
